79e3153d1ca0c7db3b3537d301741442a5a7b616
Unstopable87 <kungfudu78@gmail.com>
2025-03-25 18:48:19 +0100

    2nd commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #calculer 2 nombres
 a = int(input("entrez un nombre : "))
 b = int(input("entrez un autre nombre : "))
-result = a + b
+c = int(input("entrez un dernier nombre : "))
+result = a + b + c
 print(result)
